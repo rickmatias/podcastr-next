bebd3983fd069d9d27499398de32a5f2b0b442f8
rickmatias <rickmatias@msn.com>
2021-04-21 18:27:40 -0300

    Aula 03

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  //Necessário para uso do componente 'Image' do Next
+  //Especifica de quais domínios as imagens estão vindo
+  images: {
+    domains: ['storage.googleapis.com']
+  }
+}
